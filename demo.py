d91681417154c9e1b995200da9486c67fb184c4a
coder-utshab <ghoshutshob470@gmail.com>
2026-03-14 00:05:47 +0530

    up

---
+++ demo.py
@@ -0,0 +1,154 @@
+# name = "Utshab Kumar"
+# age = 24
+# cgpa = 4.46
+
+# print("My name is " + name)
+# print("I live in Dhaka.")
+# print("I am currently", age, "years old.")
+# print("At the age of", age, "I have started learning Python.")
+# print(name + " scored", cgpa, "in the secondary exam.")
+
+
+
+
+# print ("Utshab" + "sajib" + "shuvo")
+# print("Utshab", "sajib", "shuvo")
+
+
+
+
+# a = 18
+# b = 4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+
+
+# print(4**3)
+
+
+
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# gpa = input("Enter your gpa: ")
+
+# print("Student information")
+# print("-----------")
+# print("Name : "+name)
+# print("Age : "+age)
+# print("GPA : "+gpa) 
+
+
+
+
+# num1 = int(input("Enter first number :"))
+# num2 = int(input("Enter secoend number :"))
+
+# result = num1 + num2
+# print("The result is result",result)
+
+# result = num1 - num2
+# print("The result is result",result)
+
+# result = num1 * num2
+# print("The result is result",result)
+
+
+
+
+
+# triangle area calculation 
+
+# base = float(input("Enter Base = "))
+# height = float(input("Enter height = "))
+# area = 0.5 * base * height
+# print(area)
+
+
+
+# radius = float(input("Enter Radius = "))
+# area = 3.1416 * radius * radius
+# print("Areaof circle = ",area)
+
+
+
+
+
+
+# print(max(20,10))
+
+# num = 20.5
+# print(type(num))
+
+# print(30>20)
+# print(30<20)
+# print(30>=20)
+# print(30<=20)
+# print(30==20)
+# print(30!=20)
+
+
+
+
+# mark = 20
+# if mark>=33:
+#     print("pass")
+#     print("Congratulation")
+# print("Program end")
+
+
+# if 7>3:
+#     if 7>8:
+#         print("Hi")
+#     else:
+#         print("Hellow")
+
+
+
+
+# num1 = 30
+# num2 = 80
+# num3 = -40
+
+# if num1 > num2:
+#     if num1 > num3:
+#         print(num1)
+#     else:
+#         print(num3)
+
+# if num2 > num1:
+#     if num2 > num3:
+#         print(num2)
+#     else:
+#         print(num3)
+
+
+
+
+
+
+# ch = 'w'
+
+# if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+#     print("vowel")
+# else:
+#     print("Consonent")
+
+
+
+
+
+
+
+# মানে marks যদি 80 থেকে 100 এর মধ্যে হয়, তাহলে "A" print হবে।
+
+# marks = 75
+# if 80 <= marks <= 100:
+#     print("A")
+
+# elif 70 <= marks <= 79:
+#     print("B")
---
+++ fork.py
@@ -0,0 +1,4 @@
+n=input()
+for i in range(0,5):
+    for j in range(i+1,5):
+        print("*",end=" ")
