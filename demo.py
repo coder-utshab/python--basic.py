b0e7dc7ba1ff46625bfee7a383c57fa598deed3b
coder-utshab <ghoshutshob470@gmail.com>
2026-03-14 22:17:59 +0530

    up

--- demo.py
+++ demo.py
@@ -159,8 +159,17 @@
 
 # while loop korar system
 
-i = 1 
-while i<=30:
-    print("Bangladesh")
-    i = i + 1
-print("utshab")
+# i = 1 
+# while i<=100:
+#     print(i)
+#     i = i + 2
+# print("utshab")
+
+
+n  = int(input("Enter the last term : "))
+sum = 0
+i = 1
+while i <= n :
+    sum = sum + i
+    i = i + 4
+print(sum)
