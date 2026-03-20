07245b6a5333c79f8608b5d7e9f8b69e4845a138
coder-utshab <ghoshutshob470@gmail.com>
2026-03-20 23:52:41 +0530

    up

--- set-basic.py
+++ set-basic.py
@@ -1,6 +1,15 @@
 # set_basic.py
 
+
+# order matter করে না ❌
+
+# duplicate থাকলে remove হয়ে যায় ✔️
+
+# 👉 internally Python এটাকে hash table দিয়ে store করে
+# 👉 তাই search খুব fast (O(1))
+
 num1 = {1, 2, 3, 0, 5, 6}
 num2 = set([4, 5, 6])
 
 print(4 not in num1)
+
